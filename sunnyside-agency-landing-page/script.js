805c64e941c1b48d4a5012ca75d1484bc4dc8391
Jeff Cooper <jefcooper@gmail.com>
2022-11-23 11:34:52 -0500

    dismiss mobile menu when click outside

--- sunnyside-agency-landing-page/script.js
+++ sunnyside-agency-landing-page/script.js
@@ -1,7 +1,9 @@
 const toggleModal = (el) => {
-  if (el.checked) {
+  const inputEl = document.getElementById("header-nav__toggle");
+  if (!!el) {
     document.body.classList.add("modal");
   } else {
     document.body.classList.remove("modal");
+    inputEl.checked = false;
   }
 };
