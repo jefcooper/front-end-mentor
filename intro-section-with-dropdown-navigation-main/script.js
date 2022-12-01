9b7b9c1c78e79c8322300d5e892bf88c87ad18d3
Jeff Cooper <jefcooper@gmail.com>
2022-12-01 11:10:20 -0500

    modal, checking, unchecking, click outside all working

---
+++ intro-section-with-dropdown-navigation-main/script.js
@@ -0,0 +1,33 @@
+function uncheckDropdowns(target) {
+  const dropdowns = Array.from(
+    document.getElementsByClassName("dropdown__toggle")
+  );
+  dropdowns.forEach((el) => {
+    if (el !== target) {
+      el.checked = false;
+    }
+  });
+}
+
+function dismissModal(event, el) {
+  if (event.target === el) {
+    document.getElementById("header-nav__toggle").checked = false;
+
+    // also close all the submenus so it looks good on next open
+    uncheckDropdowns();
+    hideModal();
+  }
+}
+
+function showModal() {
+  document.body.classList.add("modal");
+}
+
+function hideModal() {
+  document.body.classList.remove("modal");
+  uncheckDropdowns();
+}
+
+function selectDropdown(event) {
+  uncheckDropdowns(event.target);
+}
