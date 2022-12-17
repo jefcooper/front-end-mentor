8d547879f87ffbb07aae984f355ec526057b38be
Jeff Cooper <jefcooper@gmail.com>
2022-12-16 22:45:36 -0500

    accordion javascript implemented

--- coffeeroasters-subscription-site/script.js
+++ coffeeroasters-subscription-site/script.js
@@ -1,3 +1,5 @@
+let coffeeOptions = {};
+
 function headerNavMenuClicked(evt) {
   console.log("headerNavMenuClicked");
   const menuButton = evt.target.closest(".menu__toggle");
@@ -41,4 +43,37 @@ function closeMenu(evt) {
 
   // register event handler for animationend
   // TODO - when animation added
+}
+
+function coffeeOption(evt, category, selection) {
+  const option = evt.target.closest(".accordion__option");
+  const accordion = evt.target.closest(".accordion");
+
+  if (option.classList.contains("accordion__option--selected")) {
+    // if we're de-selecting, then remove from dictionary
+    delete coffeeOptions[category];
+    option.classList.remove("accordion__option--selected");
+  } else {
+    // if we're selecting, set selected and add to dictionary
+    coffeeOptions[category] = selection;
+    option.classList.add("accordion__option--selected");
+  }
+  // for all other options under this accordion, de-select them
+  Array.from(accordion.children).forEach((element) => {
+    if (element !== option) {
+      element.classList.remove("accordion__option--selected");
+    }
+  });
+}
+
+function accordionClick(evt) {
+  const accordion = evt.target.closest(".accordion");
+
+  if (accordion) {
+    if (accordion.getAttribute("aria-expanded") === "true") {
+      accordion.setAttribute("aria-expanded", "false");
+    } else {
+      accordion.setAttribute("aria-expanded", "true");
+    }
+  }
 }
